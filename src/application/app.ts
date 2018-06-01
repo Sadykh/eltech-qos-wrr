e76979e5cda673b2de214f89cbafbb0e0b940fbc
Sadykh <sadykh.sadykhov@ya.ru>
2018-06-01 14:32:20 +0300

    Причесан внешний вид и задана скорость анимации и инициализации данных

--- src/application/app.ts
+++ src/application/app.ts
@@ -2,14 +2,54 @@ import {Priority} from "./model/Priority";
 import {Program} from "./model/Program";
 import {ProgramPacket} from "./model/ProgramPacket";
 import {Queue} from "./model/Queue";
+import * as $ from "jquery";
+
+/**
+ * Инциализация данных
+ */
+function init() {
+    const programList: Array<Program> = Program.init(priorityItems);
+    const packetList: Array<ProgramPacket> = ProgramPacket.init(programList);
+    priorityItems = Priority.attachPackets(priorityItems, packetList);
+}
 
 let priorityItems: Array<Priority> = Priority.init();
-const programList: Array<Program> = Program.init(priorityItems);
-const packetList: Array<ProgramPacket> = ProgramPacket.init(programList);
-priorityItems = Priority.attachPackets(priorityItems, packetList);
+init();
 
-let queue = new Queue();
+$(document).on("change", ".priority-input-event", function (e) {
+    let id = $(this).attr('data-id');
+    let value = $(this).val();
+    priorityItems[id].setWeight(Number(value));
+});
 
-for (let i = 0; i < 20; i++) {
-    console.info(queue.get());
-}
+let inProcess = 0;
+$(document).on("click", ".send-packets", function (e) {
+    e.preventDefault();
+    if (inProcess) {
+        return false;
+    }
+    inProcess = 1;
+    let initRepeat = 0;
+    for (let i = 1; i < 5; i++) {
+        if ($('#priorityPacketsQueue-column-' + i + 'ul li').length) {
+            initRepeat = 0;
+        }
+    }
+    if (initRepeat) {
+        init();
+    }
+    let queue = Queue.init(priorityItems);
+    for (let i = 0; i < 100; i++) {
+        setTimeout(function () {
+            const element = queue.get();
+            const packet = element.getPacket();
+            if (packet) {
+                packet.sendElement();
+            }
+            if (i == 100) {
+                inProcess = 0;
+            }
+        }, 1000 * i);
+
+    }
+});
